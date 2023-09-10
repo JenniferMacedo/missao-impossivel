70c7ba4030f0fe26f028cf56667dc09bc5bb9240
JenniferMacedo <jennifermacedo948@gmail.com>
2023-09-10 16:37:40 -0300

    primeiro commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import { DiGithubBadge } from "react-icons/di"
+import "./style.css"
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <a>
+      </a>
+      <h4>Trabalho iniciado em sala de aula com ajuda do Professor Mateus Chinês</h4>
+      <p>Desenvolvido por: Jénifer Macedo</p>
+
+      <a href="https://github.com/JenniferMacedo" target="blank"><DiGithubBadge/></a>
+
+    </div>
+  )
+}
